acff4b7a10dfc40b7393df79871866f31eacc286
Angel Avelar-Bonilla <55155769+AngelAvelarBonilla@users.noreply.github.com>
2020-12-02 20:51:34 -0500

    main file
    project documentation like requirements, names, etc.

---
+++ electionsystem.c
@@ -0,0 +1,19 @@
+// electionsystem.c
+// CS211 Group Project:
+//
+// Election System Users can enter choice as per preference and
+// can calculate total votes and leading candidate. Useful for
+// small scale election projects.
+//
+// Collaborators:   Angel Avelar-Bonilla
+//                  Aidan Murphey
+//                  Joanna Santos
+//                  David Witte
+//
+
+#include <stdio.h>
+
+int main(){
+
+return 0;
+}
