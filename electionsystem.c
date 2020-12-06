c59bcb55bb4cb0a2132f03572474aa11065406a1
Angel Avelar-Bonilla <55155769+AngelAvelarBonilla@users.noreply.github.com>
2020-12-06 11:50:52 -0500

    Added showResults function
    Added showResults function to tally up the current amount of votes for each candidate and list them
    Added getOfficerPosition to retrieve the name for the respective position
    Added a few options to help screen

--- electionsystem.c
+++ electionsystem.c
@@ -15,12 +15,14 @@
 #include <ctype.h>
 #include <string.h>
 
+int votes[3][3] = {{0}};
+
 void pollUserForInputFileName(char fileName[], int *fileNameLength) {
 	int hasFile = 0;
-	
+
 	printf("Do you have a input file to supply? (1=yes, 0=no): ");
 	scanf(" %d", &hasFile);
-	
+
 	if (hasFile) {
 		printf("Please enter the file name (Up to 64 characters): ");
 		scanf(" %s", fileName);
@@ -32,27 +34,56 @@ void commandHelp() {
 	puts("Help Menu (Format: Command - Description)");
 	puts("----------");
 	puts("h - Help Menu");
+	puts("v - Cast your vote");
+	puts("r - Show Results");
 	puts("e - Exit Program"); //Done
 	puts("");
 }
 
+void showResults() {
+    int i, j;
+    for(i = 0; i<3; i++){
+        getOfficerPosition(i);
+        for(j=0; j<3; j++){
+            printf("\nCandidate %d: %d votes", j+1, votes[i][j]);
+        }
+        puts("\n----------");
+    }
+}
+
+void getOfficerPosition(int i){
+    switch(i) {
+    case 0:
+        printf("President");
+        break;
+    case 1:
+        printf("Vice President");
+        break;
+    case 2:
+        printf("Treasurer");
+        break;
+    }
+}
+
 int main() {
 	char command = 'h'; //Default to 'help' so we can automatically show the user the help menu
-	
+
 	//Main loop
 	do {
 		if (command == 'h') {
 			commandHelp();
+		} else if (command == 'r'){
+		    showResults();
 		} else if (command == 'e') {
 			puts("Have a wonderful day");
 			break;
 		}
-		
+
 		printf("Please enter a command: ");
 		scanf(" %c", &command);
 		command = tolower(command);
 	} while (1); //Never-ending expression in the while here because we want to break out after we process the goodbye message. This procesing happens at the beginning of the loop, not the end
-	
+
 
 	return 0;
 }
