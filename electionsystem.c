27b4cdc73e588fc536a96f119704d96f1d9c40f9
wittedavid <47053358+wittedavid@users.noreply.github.com>
2020-12-07 22:49:09 -0500

    Add files via upload
    writing to file works, but reading does not.

--- electionsystem.c
+++ electionsystem.c
@@ -15,29 +15,29 @@
 #include <ctype.h>
 #include <string.h>
 
-#define TOTALPOSSIBLECAN 20 
-#define MAXSTRINGLENGTH 65
+#define TOTALPOSSIBLECAN 20 // total possible candidates in the array
+#define MAXSTRINGLENGTH 65  
 char candidate[TOTALPOSSIBLECAN][MAXSTRINGLENGTH]; // array that contains the string of candidate names
-char positionName[TOTALPOSSIBLECAN][MAXSTRINGLENGTH]; 
 int voteCount[TOTALPOSSIBLECAN]; // array that contains the number of votes for each candidate
-char name[MAXSTRINGLENGTH]; // name of position to be voted on ex 'President'
+char positionName[MAXSTRINGLENGTH]; // name of position to be voted on ex 'President'
 int numOfCandidates;  // number of candidates running in election
 int totalVotesCast; 
+int fileNameLength = 0;
+char fileName[MAXSTRINGLENGTH];
 
-//int fileNameLength = 0;
+void pollUserForInputFileName(char userInputNameOfFile[], int* userInputNameLength) {
+  int hasFile = 0;
 
-// void pollUserForInputFileName(char userInputNameOfFile[], int* userInputNameLength) {
-//   int hasFile = 0;
-
-//   printf("Do you have a input file to supply? (1=yes, 0=no): ");
-//   scanf(" %d", &hasFile);
-
-//   if (hasFile) {
-//     printf("Please enter the file name (Up to 64 characters): ");
-//     scanf(" %s", userInputNameOfFile);
-//     (*userInputNameLength) = strlen(userInputNameOfFile);
-//   }
-// }
+  printf("Do you have a input file to supply? (1=yes, 0=no): ");
+  scanf(" %d", &hasFile);
+  getchar(); // consumes \n from scanf
+  if (hasFile) {
+    printf("Please enter the file name (Up to 64 characters): ");
+    scanf(" %s", userInputNameOfFile);
+    getchar();
+    (*userInputNameLength) = strlen(userInputNameOfFile);
+  }
+}
 
 void commandHelp() {
   puts("Help Menu (Format: Command - Description)");
@@ -57,51 +57,39 @@ void createCandidate () {
     for (int i = 0; i < numOfCandidates; i++) { 
         printf("Please enter candidate name: "); 
         fgets(candidateName, MAXSTRINGLENGTH, stdin);
-        //printf("%s has been entered", candidateName); // test
+        strtok(candidateName, "\n");                  // eliminates \n buffer
+        printf("Candidate %s has been entered on the ballot.\n", candidateName); 
         strcpy(candidate[i], candidateName); // inserts candidate string name into candidate array
-        strtok(candidate[i], "\n");
     }
     puts("All candidates created. Ballot is now complete.");
 }
 
 void createPosition() {
   printf("What is the name of the position for the election? ");
-  fgets(name, MAXSTRINGLENGTH, stdin);
-  strtok(name, "\n");
-  printf("How many candidates are there? ");
+  fgets(positionName, MAXSTRINGLENGTH, stdin);
+  strtok(positionName, "\n");
+  printf("How many candidates are there? (min 2, max %d): ", TOTALPOSSIBLECAN);
   scanf("%d", &numOfCandidates);
   getchar();
-
-  for (int i = 0; i < numOfCandidates; i++) {
-    strcpy(positionName[i], name);
-  }
   createCandidate();
-
 }
 
-
-
-
-// void saveResults() {
-//   FILE *fp;
-//   fp = fopen(fileName, "w");
-
-//   for (int i = 0; i < 3; i++) {
-//     for (int j = 0; j < 3; j++) {
-//       fprintf(fp, "%d,%d,%d", i, j, votes[i][j]);
-
-//       //This prints a \n at the end of every line except for the last. We don't want the file to end with a newline b/c that messes up the processing when the data is read in on program startup
-//       if (!(i == 2 && j == 2)) {
-//         fprintf(fp, "\n");
-//       }
-//     }
-//   }
-
-//   fclose(fp);
-// }
+void saveResults() {
+  FILE *fp;
+  fp = fopen(fileName, "w");
+  fprintf(fp, "%s\n", positionName);
+  for (int i = 0; i < numOfCandidates; i++) {
+      fprintf(fp, "%d,%s,%d", i, candidate[i], voteCount[i]);
+      //This prints a \n at the end of every line except for the last. We don't want the file to end with a newline b/c that messes up the processing when the data is read in on program startup
+      if (!(i == 2)) {
+        fprintf(fp, "\n");
+      }
+  }
+  fclose(fp);
+}
 
 void printBallot() {
-  printf("----------- %s BALLOT ----------- \n", name);
+  printf("----------- %s BALLOT ----------- \n", positionName);
   for (int i = 0; i < numOfCandidates; i++) {
     printf("%d\t%s\n", i, candidate[i] );
   }
@@ -109,9 +97,7 @@ void printBallot() {
 
 void castVote(){
   int input;
-  
   while (input != -1) {
-
         printBallot();
         printf("Enter the number to vote for candidate. Enter -1 to complete election. -> ");
         scanf("%d", &input);
@@ -124,11 +110,10 @@ void castVote(){
         } else {
           puts("Election Complete!");
         }
-        
-        puts("\n-----------------------------------"); 
+        puts("\n----------------------------------------"); 
     }   
     
-  //saveResults();
+  saveResults();
 }
 
 void showResults() {
@@ -145,18 +130,18 @@ void showResults() {
   }
   percentage = (float)max / totalVotesCast * 100.0;
   puts("------- ELECTION RESULTS -------");
-  printf("%s Candidate %s has won the election with %d votes out of the %d total votes casted. \n", name, candidate[pos], max, totalVotesCast);
+  printf("%s Candidate %s has won the election with %d votes out of the %d total votes casted. \n", positionName, candidate[pos], max, totalVotesCast);
   printf("They won %0.2f percent of votes.", percentage);
 
   puts("\n --- Complete Election Results ---");
   for (i = 0; i < numOfCandidates; i++) { 
-        printf("%s\t\tVotes: %d\n", candidate[i], voteCount[i]);
+        printf("%s\t\t\tVotes: %d\n", candidate[i], voteCount[i]);
   }
 }
 
 void showLeadingCandidate()
 {
-    printf("\nLeading candidates for %s Election: \n", name);
+    printf("\nLeading candidates for %s Election: \n", positionName);
     int i;
     int max = voteCount[0];
     int pos = 0;
@@ -166,56 +151,60 @@ void showLeadingCandidate()
         pos = i;
         max = voteCount[i];
       }
-    }
-    
-    printf("%s Candidate %s is leading with %d votes. \n", name, candidate[pos], max);
+    } 
+    printf("%s Candidate %s is leading with %d votes. \n", positionName, candidate[pos], max);
     puts("\n----------");
 }
 
-// void readInResults() {
-//   FILE *fp;
-//   fp = fopen(fileName, "r");
+void readInResults() {
+  FILE *fp;
+  fp = fopen(fileName, "r");
 
-//   while (!feof(fp)) {
-//     int position, candidate, voteCount;
-//     fscanf(fp, "%d,%d,%d", &position, &candidate, &voteCount);
-//     votes[position][candidate] = voteCount;
-//   }
-// }
+  while (!feof(fp)) {
+    int readNum, readVote;
+    char readCandidate[MAXSTRINGLENGTH];
+    fgets(positionName, MAXSTRINGLENGTH, fp);
+    //strtok(positionName, "\n");
+    fscanf(fp, "%d,%s,%d", &readNum, &readCandidate, &readVote);
+    strcpy(candidate[readNum], readCandidate);
+    voteCount[readNum] = readVote;
 
-// void attemptToImportFile() {
-//   char userInputNameOfFile[65];
-//   int userInputNameLength = 0;
+  }
+}
 
-//   pollUserForInputFileName(userInputNameOfFile, &userInputNameLength);
-//   if (userInputNameLength == 0) {
-//     fileName[0] = 'r';
-//     fileName[1] = 'e';
-//     fileName[2] = 's';
-//     fileName[3] = 'u';
-//     fileName[4] = 'l';
-//     fileName[5] = 't';
-//     fileName[6] = 's';
-//     fileName[7] = '.';
-//     fileName[8] = 'c';
-//     fileName[9] = 's';
-//     fileName[10] = 'v';
-//     fileName[11] = '\0';
+void attemptToImportFile() {
+  char userInputNameOfFile[MAXSTRINGLENGTH];
+  int userInputNameLength = 0;
 
-//     fileNameLength = 11;
-//   } else {
-//     for (int i = 0; i <= userInputNameLength; i++) {
-//       fileName[i] = userInputNameOfFile[i];
-//     }
-//     fileNameLength = userInputNameLength;
+  pollUserForInputFileName(userInputNameOfFile, &userInputNameLength);
+  if (userInputNameLength == 0) {
+    fileName[0] = 'r';
+    fileName[1] = 'e';
+    fileName[2] = 's';
+    fileName[3] = 'u';
+    fileName[4] = 'l';
+    fileName[5] = 't';
+    fileName[6] = 's';
+    fileName[7] = '.';
+    fileName[8] = 'c';
+    fileName[9] = 's';
+    fileName[10] = 'v';
+    fileName[11] = '\0';
 
-//     readInResults();
-//   }
-// }
+    fileNameLength = 11;
+  } else {
+    for (int i = 0; i <= userInputNameLength; i++) {
+      fileName[i] = userInputNameOfFile[i];
+    }
+    fileNameLength = userInputNameLength;
+
+    readInResults();
+  }
+}
 
 int main() {
   char command = 'h'; //Default to 'help' so we can automatically show the user the help menu
-  //attemptToImportFile();
+  attemptToImportFile();
 
   //Main loop
   do {
@@ -227,9 +216,8 @@ int main() {
       showResults();
     } else if (command == 'l'){
       showLeadingCandidate();
-    }
-      else if (command == 'i') {
-      //attemptToImportFile();
+    } else if (command == 'i') {
+      attemptToImportFile();
     } else if (command == 'v') {
       castVote();
     } else if (command == 'e') {
